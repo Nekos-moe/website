3ac0d6031bc379c19260900ca859de8ec3bebe6c
Shumatsu <Shumatsu@users.noreply.github.com>
2017-12-06 10:05:48 +0100

    Danbooru tag import improvement (#3)
    * Tag import improvement
    
    Added function to change tags like "1girl" to add a space and change to "1 girl" format.
    
    * Code improvements
    
    Attached the replace to proper function and improved so it works with tags like `6+girls`.
    
    * Remove split
    
    Maybe in version 7 it will work better...

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -216,7 +216,7 @@ export default {
 					tags += ' ' + response.data.tag_string_character;
 
 				this.details.tags = tags
-					.replace(/ +/g, ', ').replace(/_/g, ' ');
+					.replace(/ +/g, ', ').replace(/_/g, ' ').replace(/([0-9]\+?)(girls?|boys?|koma)/g, '$1 $2');
 				this.details.artist = response.data.tag_string_artist.replace(/_/g, ' ');
 			}).catch(error => {
 				this.$Progress.fail();
